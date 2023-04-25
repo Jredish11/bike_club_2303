ce31ec3830024e7be07f59ba73d3565a968f7583
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-25 09:52:59 -0600

    test: Add test for log_ride method

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -7,58 +7,40 @@ RSpec.describe Biker do
     it "exists, has attributes" do
       biker = Biker.new("Kenny", 30)
       expect(biker).to be_a(Biker)
+      expect(biker.name).to eq("Kenny")
+      expect(biker.max_distance).to eq(30)
+    end
+
+    it "has an empty list of rides, acceptable terrain" do
+      biker = Biker.new("Kenny", 30)
+      expect(biker.rides).to eq({})
+      expect(biker.acceptable_terrain).to eq([])
+    end
+
+    it "can learn new terrain" do
+      biker = Biker.new("Kenny", 30)
+      biker.learn_terrain!(:gravel)
+      biker.learn_terrain!(:hills)
+      expect(biker.acceptable_terrain).to eq([:gravel, :hills])
+    end
+
+    it "log a ride with a time. The Biker can keep track of all of its previous rides and times for those rides" do
+      biker = Biker.new("Kenny", 30)
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+      biker.log_ride(ride1, 92.5)
+      biker.log_ride(ride1, 91.1)
+      biker.log_ride(ride2, 60.9)
+      biker.log_ride(ride2, 61.6)
+      expect(biker.rides).to eq({
+                                ride1 => [92.5, 91.1],
+                                ride2 => [60.9, 61.6]
+                                })
     end
   end
 end
 
 
-
-
-
-
-
-
-# biker = Biker.new("Kenny", 30)
-# # => #<Biker:0x00007fc62ca41150...>
-
-# biker.name
-# # => "Kenny"
-
-# biker.max_distance
-# # => 30
-
-# biker.rides
-# # => {}
-
-# biker.acceptable_terrain
-# # => []
-
-# biker.learn_terrain!(:gravel)
-
-# biker.learn_terrain!(:hills)
-
-# biker.acceptable_terrain
-# # => [:gravel, :hills]
-
-# ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
-# # => #<Ride:0x00007fc62ca32a10...>
-
-# ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
-# # => #<Ride:0x00007fc62cb42ba8...>
-
-# biker.log_ride(ride1, 92.5)
-
-# biker.log_ride(ride1, 91.1)
-
-# biker.log_ride(ride2, 60.9)
-
-# biker.log_ride(ride2, 61.6)
-
-# biker.rides
-# # => {
-# #      #<Ride:0x00007fc62ca32a10...> => [92.5, 91.1],
-# #      #<Ride:0x00007fc62cb42ba8...> => [60.9, 61.6]
-# #    }
 
 # biker.personal_record(ride1)
 # => 91.1
