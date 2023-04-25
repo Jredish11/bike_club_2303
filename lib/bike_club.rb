629aefdbe0a7b80309c666e7a70d2bec7a9acf45
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-25 11:15:40 -0600

    feat: Add most_rides method, and add_biker

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,8 +1,16 @@
 class BikeClub
-  attr_reader :name
+  attr_reader :name, :list
   def initialize(name)
     @name = name
+    @list = []
+  end
 
+  def add_biker(biker)
+    list << biker
+  end
+
+  def most_rides
+    list.max_by { |biker| biker.rides.count }
   end
   
 end
