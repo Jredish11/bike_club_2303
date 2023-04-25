6237a8e40e2da40c21ac9bb34694fb23c1478a74
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-25 09:53:14 -0600

    feat: Add log_ride method

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,6 +1,25 @@
 class Biker
+  attr_reader :name, 
+              :max_distance,
+              :rides,
+              :acceptable_terrain
   def initialize(name, max_distance)
-    
+    @name = name
+    @max_distance = max_distance
+    @rides = Hash.new
+    @acceptable_terrain = []
+  end
+
+  def learn_terrain!(terrain)
+    acceptable_terrain << terrain
+  end
+
+  def log_ride(ride, personal_record)
+    if rides[ride].nil?
+      rides[ride] = [personal_record]
+    else
+      rides[ride] << personal_record
+    end
   end
   
 end
