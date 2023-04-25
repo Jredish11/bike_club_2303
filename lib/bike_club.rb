bad12b0ec3b634597ca6fe46246c8f93ad7abeab
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-25 11:47:22 -0600

    feat: Add best_time method, start bikers_eligble

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -16,5 +16,14 @@ class BikeClub
   def best_time(ride)
     list.min_by { |biker| biker.personal_record(ride) }
   end
+
+  def bikers_eligible(ride)
+    # A Biker is eligible for a Ride if the terrain is acceptable to them and the total distance does not exceed their max distance.
+    list.each do |biker|
+     biker.rides.map do |rides, record| 
+      biker.log_ride(ride, record)
+     end
+    end
+  end
   
 end
