78ab965e8e4b6a5e13f88490bb0d804655a06925
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-25 11:32:58 -0600

    test: Add tests for best_time

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -38,10 +38,30 @@ RSpec.describe BikeClub do
       biker2.learn_terrain!(:gravel)
       biker2.learn_terrain!(:hills)
       biker1.log_ride(ride1, 92.5)
-      biker1.log_ride(ride2, 60.9)
-      biker2.log_ride(ride2, 61.6)
+      biker2.log_ride(ride2, 60.9)
+      biker1.log_ride(ride2, 61.6)
 
       expect(bike_club.most_rides).to eq(biker1)
+    end
+
+    it "can tell us which Biker has the best time for a given Ride" do
+      bike_club = BikeClub.new("2 Wheel Mafia")
+      biker1 = Biker.new("Kenny", 30)
+      biker2 = Biker.new("Athena", 15)
+      bike_club.add_biker(biker1)
+      bike_club.add_biker(biker2)
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+      biker1.learn_terrain!(:gravel)
+      biker1.learn_terrain!(:hills)
+      biker2.learn_terrain!(:gravel)
+      biker2.learn_terrain!(:hills)
+      biker1.log_ride(ride1, 92.5)
+      biker2.log_ride(ride1, 69.9)
+      biker1.log_ride(ride2, 61.6)
+      biker2.log_ride(ride2, 65.9)
+      expect(bike_club.best_time(ride1)).to eq(biker2)
+      expect(bike_club.best_time(ride2)).to eq(biker1)
     end
   end
 end
@@ -54,13 +74,6 @@ end
 
 # Additionally, use TDD to add the following functionality to the BikeClub class. A passing challenge will complete at least one of the following. We recommend completing more than one if you have time.
 
-# A BikeClub can tell us which Biker has logged the most rides.
+
 # A BikeClub can tell us which Biker has the best time for a given Ride.
 # A BikeClub can tell us which Bikers are eligible for a given Ride. A Biker is eligible for a Ride if the terrain is acceptable to them and the total distance does not exceed their max distance.
-# Method Name	Return Value
-# name	Club name as a String
-# bikers	Array of Biker objects
-# add_biker(biker)	Array of Biker objects
-# most_rides	Biker object
-# best_time(ride)	Biker object
-# bikers_eligible(ride)	Array of Biker objects
