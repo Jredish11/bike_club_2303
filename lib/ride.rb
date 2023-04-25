4020fee9a5f293ab3026a1cdefe8ba12c335c26e
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-25 09:11:17 -0600

    feat: Add initialize, loop?, total_distance

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,22 @@
 class Ride
+  attr_reader :name,
+              :distance,
+              :terrain,
+              :loop
+
+  def initialize(data)
+    @name = data[:name]
+    @distance = data[:distance]
+    @terrain = data[:terrain]
+    @loop = data[:loop]
+  end
+
+  def loop?
+    @loop
+  end
+
+  def total_distance
+    distance * 2
+  end
 
 end
