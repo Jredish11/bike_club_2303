7b0526b2ebffa3a4921124815343290ceb72de25
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-25 10:08:21 -0600

    test: Add test for personal_record

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -37,16 +37,35 @@ RSpec.describe Biker do
                                 ride2 => [60.9, 61.6]
                                 })
     end
+
+    it "can check personal_record of a ride" do
+      biker = Biker.new("Kenny", 30)
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+      biker.log_ride(ride1, 92.5)
+      biker.log_ride(ride1, 91.1)
+      biker.log_ride(ride2, 60.9)
+      biker.log_ride(ride2, 61.6)
+      expect(biker.personal_record(ride1)).to eq(91.1)
+      expect(biker.personal_record(ride2)).to eq(60.9)
+    end
+
+    it "checks another biker's rides personal record" do
+      biker2 = Biker.new("Athena", 15)
+      biker2.log_ride(ride1, 97.0)
+      biker2.log_ride(ride2, 67.0)
+      expect(biker2.rides).to eq({})
+      biker2.learn_terrain!(:gravel)
+      biker2.learn_terrain!(:hills)
+      biker2.log_ride(ride1, 95.0)
+      biker2.log_ride(ride2, 65.0)
+      biker2.rides({})
+
+    end
   end
 end
 
 
-
-# biker.personal_record(ride1)
-# => 91.1
-
-# biker.personal_record(ride2)
-# => 60.9
 
 # biker2 = Biker.new("Athena", 15)
 # => #<Biker:0x00007fc62cb399e0...>
